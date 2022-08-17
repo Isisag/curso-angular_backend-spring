3c18e820058a61872918446bf7aa807c72867abe
Isis Aguilar Orellana <iaguilao@emeal.nttdata.com>
2022-08-17 07:34:12 -0400

    first commit

---
+++ src/main/java/com/nttlab/spring/boot/api/backend/BackendProyectoCursoAngularApplication.java
@@ -0,0 +1,13 @@
+package com.nttlab.spring.boot.api.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProyectoCursoAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProyectoCursoAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttlab/spring/boot/api/backend/BackendProyectoCursoAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttlab.spring.boot.api.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProyectoCursoAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
